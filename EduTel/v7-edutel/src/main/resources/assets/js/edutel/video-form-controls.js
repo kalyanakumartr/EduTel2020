d1f5842d0d067d88e582fba76b6d44ed42040f6d
Ananth <kalyanakumartr@gmail.com>
2020-10-17 21:59:07 +0530

    AlignmentFix
    Change-Id: I96bcd0fada97c0451b0b06d86272f5a1b109ca90

--- EduTel/v7-edutel/src/main/resources/assets/js/edutel/video-form-controls.js
+++ EduTel/v7-edutel/src/main/resources/assets/js/edutel/video-form-controls.js
@@ -89,7 +89,6 @@ var KTFormControls = function() {
 							
 							document.body.scrollTop = 0; // For Safari
 							document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-							//alert(localStorage.getItem("accessToken"));
 							  
 							$.ajax({
 								type : "POST",
